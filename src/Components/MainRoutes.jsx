6507b460fe950852b1db388ff2b6a405d230992f
Sushree-01 <subhashree.payal2000@gmail.com>
2023-09-27 00:01:15 +0530

    fp09_241-day-1

---
+++ src/Components/MainRoutes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Dashboard } from '../Pages/Dashboard'
+import { Login } from '../Pages/Login'
+import { Cart } from '../Pages/Cart'
+import { Payment } from '../Pages/Payment'
+import { Product } from '../Pages/Product'
+import { Signup } from '../Pages/Signup'
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/payment' element={<Payment/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/product/:id' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+  )
+}
