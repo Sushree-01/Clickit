3bc35ea2ba3b6dde79dbc08cb11e8e2c0ebebde1
Sushree-01 <subhashree.payal2000@gmail.com>
2023-09-28 23:43:53 +0530

    fp09_241-day-2

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const Product = () => {
+  const product=useSelector((state)=>state.productReducer.products)
+  console.log(product);
+  const dispatch=useDispatch()
+  
   return (
-    <div>Product</div>
+    <div>
+
+    </div>
   )
 }
